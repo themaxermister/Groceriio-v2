ef12340b2b336e76660d488673c61abd778d54e4
themaxermister <subscribingalerts@gmail.com>
2022-04-12 02:12:00 +0800

    Completed map pages

--- app/src/main/java/com/example/newgroceriio/RegisterSuccessActivity.java
+++ app/src/main/java/com/example/newgroceriio/RegisterSuccessActivity.java
@@ -3,12 +3,17 @@ package com.example.newgroceriio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RegisterSuccessActivity extends AppCompatActivity {
+    Button mOrdSuccessBackBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_success);
+
+        mOrdSuccessBackBtn = findViewById(R.id.ordSuccessBackBtn);
+
     }
 }

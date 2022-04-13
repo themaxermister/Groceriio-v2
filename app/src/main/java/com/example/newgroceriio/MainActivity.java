2c0da21615ee9088f11e757cf3d1742468a8c755
clash9529 <clash9529@gmail.com>
2022-04-14 01:44:17 +0800

    Map

--- app/src/main/java/com/example/newgroceriio/MainActivity.java
+++ app/src/main/java/com/example/newgroceriio/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.newgroceriio;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,9 +20,18 @@ import android.widget.Toast;
 import com.example.newgroceriio.Adapters.CategoryAdapter;
 import com.example.newgroceriio.Models.Category;
 import com.example.newgroceriio.Models.Product;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,6 +40,9 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements CategoryAdapter.OnCategoryListener{
     NavigationBarView mHomeNavBar;
@@ -44,6 +60,14 @@ public class MainActivity extends AppCompatActivity implements CategoryAdapter.O
     CategoryAdapter adapter;
     SharedPreferences sharedPreferences;
 
+    DatabaseReference storeRef;
+    private List<String> locationsList;
+    private LocationRequest locationRequest;
+    public static final int REQUEST_CHECK_SETTING = 1001;
+    private static boolean mLocationBool = false;
+
+    private String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,7 +81,48 @@ public class MainActivity extends AppCompatActivity implements CategoryAdapter.O
                     case R.id.homePage:
                         break;
                     case R.id.mapPage:
-                        startActivity(new Intent(getApplicationContext(), NearestStoreActivity.class));
+                        locationsList = new ArrayList<>();
+                        GetLocations();
+
+                        locationRequest = LocationRequest.create();
+                        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                        locationRequest.setFastestInterval(2000);
+
+                        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                                .addLocationRequest(locationRequest);
+                        builder.setAlwaysShow(true);
+
+                        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                                .checkLocationSettings(builder.build());
+
+                        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+                            @Override
+                            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                                try {
+                                    LocationSettingsResponse response = task.getResult(ApiException.class);
+
+                                    startActivityPage();
+                                    Toast.makeText(MainActivity.this, "Gps is on", Toast.LENGTH_SHORT).show();
+                                } catch (ApiException e) {
+                                    switch(e.getStatusCode()){
+                                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                                            try {
+                                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                                resolvableApiException.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTING);
+                                            } catch (IntentSender.SendIntentException sendIntentException) {
+                                                Log.d(TAG, "Send intent exception");
+                                            }
+                                            break;
+
+                                        //when user device does not have location functionality
+                                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                            break;
+                                    }
+                                }
+
+                            }
+                        });
                         break;
                     case R.id.cartPage:
 //                        startActivity(new Intent(getApplicationContext(), ShoppingListActivity.class));
@@ -176,6 +241,66 @@ public class MainActivity extends AppCompatActivity implements CategoryAdapter.O
 
         intent.putExtra("type", c.getCategoryType());
         startActivity(intent);
+
+    }
+
+    ////////////// For Map Below vv //////////////
+
+
+    private void startActivityPage(){
+        Intent intent = new Intent(MainActivity.this, NearestStoreActivity.class);
+        intent.putStringArrayListExtra("locations", (ArrayList<String>) locationsList);
+
+        System.out.println(mLocationBool);
+
+        new Timer().schedule(new TimerTask(){
+            public void run() {
+                startActivity(intent);
+            }
+        }, 3000);
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CHECK_SETTING){
+            switch (resultCode){
+                case Activity.RESULT_OK:
+                    Toast.makeText(this, "GPS is turned on. Press the button one more time to continue", Toast.LENGTH_SHORT).show();
+                    break;
+                case Activity.RESULT_CANCELED:
+                    Toast.makeText(this, "GPS is required to be turned on", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+    }
+
+    private void GetLocations(){
+
+        // Database Ref
+        storeRef = FirebaseDatabase.getInstance().getReference().child("store_data");
+
+        storeRef.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot s: snapshot.getChildren()){
+                    String address = s.child("Address").getValue(String.class);
+                    System.out.println(s);
+                    System.out.println(address);
+                    locationsList.add(address);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/newgroceriio/NearestStoreActivity.java
+++ app/src/main/java/com/example/newgroceriio/NearestStoreActivity.java
@@ -1,30 +1,302 @@
 package com.example.newgroceriio;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-public class NearestStoreActivity extends AppCompatActivity {
-    Button mMapSearchBackBtn;
-    TextInputEditText mMapSearchBar;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class NearestStoreActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "MapActivity";
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+
+    private EditText searchText;
+    private Button mMapSearchBackBtn;
+
+    //vars
+    private Boolean mLocationPermissionsGranted = false;
+    private FusedLocationProviderClient locClient;
+    private GoogleMap mMap;
+
+    private List<String> locationsList;
+    private List<LatLng> latLngList;
+    private Address search_Address = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearest_store);
 
-        mMapSearchBackBtn = findViewById(R.id.mapSearchBackBtn);
-        mMapSearchBar = findViewById(R.id.mapSearchBarInput);
+        latLngList = new ArrayList<>();
 
-        mMapSearchBackBtn.setOnClickListener(new View.OnClickListener(){
+        getLocationPermission();
+
+        Intent intent = getIntent();
+
+        locationsList = intent.getStringArrayListExtra("locations");
+        convertAddressToLatLng(locationsList);
+
+        searchText = findViewById(R.id.mapSearchBarInput);
+        mMapSearchBackBtn = findViewById(R.id.mapSearchBackBtn);
+
+        mMapSearchBackBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
             }
         });
+
+        // search for desired current location
+        searchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH ||
+                        actionId == EditorInfo.IME_ACTION_DONE ||
+                        keyEvent.getAction() == KeyEvent.ACTION_DOWN ||
+                        keyEvent.getAction() == KeyEvent.KEYCODE_ENTER){
+                    geoLocate();
+                }
+                return false;
+            }
+        });
+
+
+
+    }
+
+    private void geoLocate(){
+        String searchString = searchText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(NearestStoreActivity.this);
+
+        List<Address> list = new ArrayList<>();
+
+        try{
+            list = geocoder.getFromLocationName(searchString, 1);
+        }
+        catch(IOException e){
+            Log.e(TAG, "IOException:" + e.getMessage());
+        }
+
+        if(list.size() > 0){
+            System.out.println(list.get(0).toString());
+            search_Address = list.get(0);
+            moveCamera(new LatLng(search_Address.getLatitude(), search_Address.getLongitude()), 15f);
+
+            Location search_Location = new Location("");
+            search_Location.setLatitude(search_Address.getLatitude());
+            search_Location.setLongitude(search_Address.getLongitude());
+            // Sort location by distance
+            sortMarkersFromLocation(search_Location);
+
+            mMap.clear();
+            //Create markers to indicate all store locations
+            for(int i = 0; i < latLngList.size(); i++){
+                mMap.addMarker(new MarkerOptions().position(latLngList.get(i)).title(String.valueOf(i)));
+            }
+        }
+    }
+
+
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+        ActivityCompat.requestPermissions(this,
+                permissions,
+                LOCATION_PERMISSION_REQUEST_CODE);
+    }
+
+    private void initMap(){
+        Log.d(TAG, "initMap: initializing map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(NearestStoreActivity.this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Intent intent = getIntent();
+
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: map is ready");
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getDeviceLocation();
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    private void convertAddressToLatLng(List<String> locations){
+
+        List<Address> addresses = new ArrayList<>();
+
+        // Convert String address to object address
+        for(String address: locations) {
+            final Geocoder geocoder = new Geocoder(this);
+
+            try {
+                List<Address> tempAddresses = geocoder.getFromLocationName(address, 1);
+                if (tempAddresses != null && !tempAddresses.isEmpty()) {
+                    Address temp = tempAddresses.get(0);
+                    // Get all product addresses
+                    System.out.println(address);
+                    addresses.add(temp);
+                    String message = String.format("Latitude: %f, Longitude: %f", temp.getLatitude(), temp.getLongitude());
+                    Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+                } else {
+                    // Display appropriate message when Geocoder services are not available
+                    Toast.makeText(this, "Unable to geocode zipcode", Toast.LENGTH_LONG).show();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // convert object address to object latlng
+        for(int i = 0; i < addresses.size(); i++){
+            LatLng lat_lng = new LatLng(addresses.get(i).getLatitude(), addresses.get(i).getLongitude());
+            latLngList.add(lat_lng);
+        }
+    }
+
+    private void getDeviceLocation(){
+        locClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            if(mLocationPermissionsGranted){
+                @SuppressLint("MissingPermission") Task location = locClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "Found location");
+
+                            Location current_location = (Location)task.getResult();
+                            moveCamera(new LatLng(current_location.getLatitude(), current_location.getLongitude()), 15f);
+
+                            // Sort location by distance
+                            sortMarkersFromLocation(current_location);
+
+                            //Create markers to indicate all store locations
+                            for(int i = 0; i < latLngList.size(); i++){
+                                mMap.addMarker(new MarkerOptions().position(latLngList.get(i)).title(String.valueOf(i)));
+                            }
+
+                        }
+                        else{
+                            Log.d(TAG, "Cannot find location");
+                            Toast.makeText(NearestStoreActivity.this, "Location Not found", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom){
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "onRequestPermissionsResult: called.");
+        mLocationPermissionsGranted = false;
+
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionsGranted = false;
+                            Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                    mLocationPermissionsGranted = true;
+                    //initialize our map
+                    initMap();
+                }
+            }
+        }
+    }
+
+    private void sortMarkersFromLocation(Location current_location) {
+        System.out.println("Below is markers");
+
+        List<Location> markers = new ArrayList<>();
+
+        for(LatLng lat_lng: latLngList){
+            Location location = new Location("");
+            location.setLatitude(lat_lng.latitude);
+            location.setLongitude(lat_lng.longitude);
+            markers.add(location);
+        }
+
+        System.out.println(markers);
+        Collections.sort(markers, new SortDistance(){
+            @Override
+            public int compare(Location o1, Location o2) {
+                Float dist1 = o1.distanceTo(current_location);
+                Float dist2 = o2.distanceTo(current_location);
+                return dist1.compareTo(dist2);
+            }
+        });
+        System.out.println(markers);
+
+        latLngList = new ArrayList<>();
+        for(Location m: markers){
+            latLngList.add(new LatLng(m.getLatitude(), m.getLongitude()));
+        }
+
+    }
+
+}
+
+class SortDistance implements Comparator<Location> {
+
+    public int compare(Location o1, Location o2) {
+        return 0;
     }
 }

ac3f674761572d318aebffaf8cd12e5b703a48a6
clash9529 <clash9529@gmail.com>
2022-04-25 20:27:24 +0800

    Comments ProductPageActivity

--- app/src/main/java/com/example/newgroceriio/ProductPageActivity.java
+++ app/src/main/java/com/example/newgroceriio/ProductPageActivity.java
@@ -2,8 +2,6 @@ package com.example.newgroceriio;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -19,7 +17,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.newgroceriio.Models.StockValue;
-import com.example.newgroceriio.Models.Store;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -27,10 +24,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class ProductPageActivity extends AppCompatActivity {
@@ -82,6 +76,7 @@ public class ProductPageActivity extends AppCompatActivity {
         user_location.setLatitude(Double.parseDouble(latitude));
         user_location.setLongitude(Double.parseDouble(longitude));
 
+        // nearestStoreId is put into sharedPreferences in MainActivity
         nearestStoreId = sharedPreferences.getString("nearestStoreId", "");
         getStockQuantity(nearestStoreId, pId);
 
